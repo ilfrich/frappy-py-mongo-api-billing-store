07769f5e130950eeb765c58f78c6845520f9d2e0
ilfrich <das-peter@gmx.de>
2022-04-13 12:42:50 +1000

    Standardised the api of store methods

--- frappymongoapibilling/usage_store.py
+++ frappymongoapibilling/usage_store.py
@@ -1,5 +1,5 @@
 from datetime import datetime, time
-from typing import List, Union
+from typing import List, Union, Optional
 from frappyapibilling import Usage, AbstractUsageStore
 from pbu import AbstractMongoStore, AbstractMongoDocument
 
@@ -22,10 +22,25 @@ class MongoUsage(Usage, AbstractMongoDocument):
         }
 
 
+def _compose_query(client_id: Union[str, int], start_datetime: Optional[datetime],
+                   end_datetime: Optional[datetime]) -> dict:
+    # compose query
+    query = {"clientId": client_id}
+    # handle timestamps
+    if start_datetime is not None or end_datetime is not None:
+        query["timestamp"] = {}
+    if start_datetime is not None:
+        query["timestamp"]["$gte"] = round(start_datetime.timestamp())
+    if end_datetime is not None:
+        query["timestamp"]["$lte"] = round(end_datetime.timestamp())
+    return query
+
+
 class UsageStore(AbstractMongoStore, AbstractUsageStore):
     """
     MongoDB implementation of the AbstractUsageStore methods.
     """
+
     def __init__(self, mongo_url, mongo_db, collection_name):
         AbstractUsageStore.__init__(self)
         AbstractMongoStore.__init__(self, mongo_url, mongo_db, collection_name, MongoUsage)
@@ -37,27 +52,17 @@ class UsageStore(AbstractMongoStore, AbstractUsageStore):
         new_usage.credits = credits_used
         self.create(new_usage)
 
-    def get_total_usage(self, client_id, start_datetime: datetime, end_datetime: datetime) -> Union[float, int]:
+    def get_total_usage(self, client_id: Union[str, int], start_datetime: Optional[datetime] = None,
+                        end_datetime: Optional[datetime] = None) -> Union[float, int]:
         # run query
-        items = self.query({
-            "clientId": client_id,
-            "timestamp": {
-                "$gte": round(start_datetime.timestamp()),
-                "$lte": round(end_datetime.timestamp()),
-            }
-        })
+        items = self.query(_compose_query(client_id, start_datetime, end_datetime))
         # just sum up used credits overall
         return sum(list(map(lambda x: x.credits, items)))
 
-    def get_daily_usage(self, client_id, start_datetime: datetime, end_datetime: datetime) -> List[Usage]:
+    def get_daily_usage(self, client_id: Union[str, int], start_datetime: Optional[datetime] = None,
+                        end_datetime: Optional[datetime] = None) -> List[Usage]:
         # run query
-        items = self.query({
-            "clientId": client_id,
-            "timestamp": {
-                "$gte": round(start_datetime.timestamp()),
-                "$lte": round(end_datetime.timestamp()),
-            }
-        })
+        items = self.query(_compose_query(client_id, start_datetime, end_datetime))
 
         # process query result
         result_map = {}
@@ -76,3 +81,10 @@ class UsageStore(AbstractMongoStore, AbstractUsageStore):
 
         # return sorted (by timestamp) list of usage elements
         return list(sorted(result_map.values(), key=lambda x: x.timestamp))
+
+    def delete_client_usage(self, client_id: Union[str, int], start_datetime: Optional[datetime] = None,
+                            end_datetime: Optional[datetime] = None):
+        # compose query
+        query = _compose_query(client_id, start_datetime, end_datetime)
+        # finally execute the delete
+        self.delete_many(query)
